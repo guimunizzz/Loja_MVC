8a49ee203e892b29ef71d4d4314fb737024915bc
guimunizzz <guilherme.muniz5@hotmail.com>
2025-11-07 08:57:34 -0300

    formatando routes e clienteRoutes

--- src/routes/clienteRoutes.js
+++ src/routes/clienteRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const clienteRoutes = express.Router();
-const {clienteController} = require('../controllers/clienteController');
+const { clienteController } = require('../controllers/clienteController');
 
 clienteRoutes.get('/clientes', clienteController.selecionaTodos)
 clienteRoutes.post('/clientes', clienteController.incluiCliente)
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const {produtoRoutes} = require('./produtoRoutes');
-const {clienteRoutes} = require('./clienteRoutes');
+const { produtoRoutes } = require('./produtoRoutes');
+const { clienteRoutes } = require('./clienteRoutes');
 
 router.use('/', produtoRoutes);
 router.use('/', clienteRoutes);
 
-router.use((req,res) => {
+router.use((req, res) => {
     res.status(404).json({
         message: 'Pagina não encontrada'
     });
 });
 
-module.exports = {router};
+module.exports = { router };
